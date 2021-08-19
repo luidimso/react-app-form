fdb3f87ce71640ada11e2c78895586c5ced686e6
luidimso <luidi_mso@outlook.com>
2021-08-19 18:20:12 -0300

    Creating ErrorModal component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 function AddUser(props) {
     const [enteredUsername, setEnterededUsername] = useState('');
@@ -32,17 +33,21 @@ function AddUser(props) {
     }
 
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+        <div>
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
 
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
 
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+
+            <ErrorModal title="An error ocurred!" message="Something went wrong!"></ErrorModal>
+        </div>
     );
 }
 
