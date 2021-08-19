28f2d5089c2733e31bee84028f65efad9d569381
luidimso <luidi_mso@outlook.com>
2021-08-19 17:52:03 -0300

    Creating UserList component

---
+++ src/components/Users/UserList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../UI/Card';
+import classes from './UserList.module.css';
+
+function UserList(props) {
+    return (
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map(user => <li>{user.name} ({user.age} years old)</li>)}
+            </ul>
+        </Card>
+    );
+}
+
+export default UserList;
