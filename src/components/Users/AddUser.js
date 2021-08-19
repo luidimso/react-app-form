626a735db80c24dc02cef46400c6b5e0f613be56
luidimso <luidi_mso@outlook.com>
2021-08-19 17:28:47 -0300

    Handling inputs changes

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 
 function AddUser(props) {
+    const [enteredUsername, setEnterededUsername] = useState('');
+    const [enteredAge, setEnterededAge] = useState('');
+
     function addUserHandler(event) {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    }
+
+    function usernameChangeHandler(event) {
+        setEnterededUsername(event.target.value);
+    }
+
+    function ageChangeHandler(event) {
+        setEnterededAge(event.target.value);
     }
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" onChange={usernameChangeHandler} />
 
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={ageChangeHandler} />
 
                 <Button type="submit">Add User</Button>
             </form>
